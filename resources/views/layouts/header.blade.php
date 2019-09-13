1acd4d204d064c8821cb464d3bdb0dec3eac0d7a
mubeen <mahermubeenahmad@gmail.com>
2019-09-13 16:38:00 +0500

    new auth changings

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>CarGuru</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="icon" type="image/ico" href="{{ asset('images/carguru-logo.png') }}"/>
-    <script src="{{ asset('https://kit.fontawesome.com/b11236bde2.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    <link rel="icon" type="image/ico" href="{{ secure_asset('images/carguru-logo.png') }}"/>
+    <script src="{{ secure_asset('https://kit.fontawesome.com/b11236bde2.js') }}"></script>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 </head>
 <body>
