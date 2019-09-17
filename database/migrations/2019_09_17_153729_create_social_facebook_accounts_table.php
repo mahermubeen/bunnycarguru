836d64ac45598c76a14fc6305502b1c2e129614a
mahermubeen <mahermubeenahmad@gmail.com>
2019-09-17 09:59:12 -0700

    new api set

--- database/migrations/2019_09_17_153729_create_social_facebook_accounts_table.php
+++ database/migrations/2019_09_17_153729_create_social_facebook_accounts_table.php
@@ -14,7 +14,8 @@ class CreateSocialFacebookAccountsTable extends Migration
     public function up()
     {
         Schema::create('social_facebook_accounts', function (Blueprint $table) {
-            $table->bigIncrements('user_id');
+            $table->bigIncrements('id');
+            $table->integer('user_id');
             $table->string('provider_user_id');
             $table->string('provider');
             $table->timestamps();
