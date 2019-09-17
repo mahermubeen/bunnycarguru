6c3dee18812160af7ab5eca4d6e07619b4b6b5dc
mahermubeen <mahermubeenahmad@gmail.com>
2019-09-17 09:30:00 -0700

    login user changning

--- app/User.php
+++ app/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $primaryKey = 'id';
+
+    protected $incrementing = false;
+
     /**
      * The attributes that are mass assignable.
      *
