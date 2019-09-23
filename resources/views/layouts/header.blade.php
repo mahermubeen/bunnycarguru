be4968b4693175ae4d5903f78bf818f462537a36
mahermubeen <mahermubeenahmad@gmail.com>
2019-09-23 09:54:42 -0700

    master changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
+
 <div class="wrapper">
     <header class="header">
         <div id="main-nav" class="responsive-nav">
