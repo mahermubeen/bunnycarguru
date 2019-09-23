7cd569284c4a8d810034a76bbf50d07a2964df0b
mahermubeen <mahermubeenahmad@gmail.com>
2019-09-23 09:49:38 -0700

    again bubble cart

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 </head>
 <body>
-
 <div class="wrapper">
     <header class="header">
         <div id="main-nav" class="responsive-nav">
